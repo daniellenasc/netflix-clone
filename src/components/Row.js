9c8e5de9a2da0f29ee35420efe7114707a54f515
Danielle Nascimento <danielle_nascimento_@hotmail.com>
2022-09-23 21:51:31 +0200

    fix function Row

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,28 +1,24 @@
-import { React, useEffect, useState } from "react"
+import React, { useEffect, useState } from "react";
 import { getMovies } from "../api";
 
-function Row(title, path) {
-    const [movies, setMovies] = useState([]);
+function Row({ title, path }) {
+  const [movies, setMovies] = useState([]);
 
-    const fetchMovies = async (_path) => {
-        try {
-          const data = await getMovies(_path);
-          console.log("data ", data);
-          setMovies(data?.results);
-        } catch (error) {
-          console.error("fetchMovies error: ", error);
-        }
-      };
-    
-      useEffect(() => {
-        fetchMovies(path);
-      }, [path]);
+  const fetchMovies = async (_path) => {
+    try {
+      const data = await getMovies(_path);
+      console.log("data ", data);
+      setMovies(data?.results);
+    } catch (error) {
+      console.error("fetchMovies error: ", error);
+    }
+  };
 
+  useEffect(() => {
+    fetchMovies(path);
+  }, [path]);
 
-
-    return (
-        <div>Row</div>
-    )
+  return <div>Row</div>;
 }
 
-export default Row
+export default Row;
